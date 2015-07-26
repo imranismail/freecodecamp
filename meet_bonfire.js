ec6ec172f95c6360069f8e81a9963aa19be983dd
Imran Ismail <imran.codely@gmail.com>
2015-07-26 16:31:53 +0800

    Meet Bonfire

---
+++ meet_bonfire.js
@@ -0,0 +1,10 @@
+/**
+ * @param  {String}
+ * @return {Boolean}
+ */
+function meetBonfire(argument) {
+  console.log("you can read this function's argument in the developer tools", argument);
+  return true;
+}
+
+meetBonfire("You can do this!");
