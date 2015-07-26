8e9b5bce1dcc6e121b4a2d3ad721ac6acefd9b61
Imran Ismail <imran.codely@gmail.com>
2015-07-26 16:37:20 +0800

    Sum all numbers in range

---
+++ sum_all_numbers_in_range.js
@@ -0,0 +1,18 @@
+/**
+ * @param  {Array}
+ * @return {Number}
+ */
+function sumAll(arr) {
+  var maxNum = Math.max(arr[0], arr[1]);
+  var minNum = Math.min(arr[0], arr[1]);
+  var sum = 0;
+
+  for (var i = minNum; i <= maxNum; i++) {
+    sum += i;
+  }
+
+  console.log(sum);
+  return sum;
+}
+
+sumAll([1, 4]);
