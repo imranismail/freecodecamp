234e29af979ee870068e99edb22f74df06ab8b9f
Imran Ismail <imran.codely@gmail.com>
2015-07-26 16:33:15 +0800

    Title Case a sentence

---
+++ title_case_a_sentence.js
@@ -0,0 +1,14 @@
+/**
+ * @param  {String}
+ * @return {String}
+ */
+function titleCase(str) {
+  str = str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+
+  console.log(str);
+  return str;
+}
+
+titleCase("I'm a little tea pot");
