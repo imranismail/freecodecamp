2412c56b5236c9135a70be8ba9269301189a837c
Imran Ismail <imran.codely@gmail.com>
2015-07-28 03:29:51 +0800

    included alternative solution for diff_two_arrays

--- diff_two_arrays.js
+++ diff_two_arrays.js
@@ -14,3 +14,43 @@ function diff(a, b) {
 }
 
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+/**
+ * Alternative solution
+ * @param  {Array} a array A
+ * @param  {Array} b array A
+ * @return {Array}   unique elements between the two arrays
+ */
+function diff(arr1, arr2) {
+  "use strict";
+  var
+    lookupList = {},
+    result = [],
+    jointArray = arr1.concat(arr2);
+
+  jointArray.forEach(function(element) {
+    if (lookupList[element] === undefined) {
+      lookupList[element] = {
+        value: element,
+        unique: true
+      };
+    } else {
+      lookupList[element] = {
+        value: element,
+        unique: false
+      };
+    }
+  });
+
+  for (var item in lookupList) {
+    if (lookupList[item].unique) {
+      result.push(lookupList[item].value);
+    }
+  }
+
+  console.log(result);
+  return result;
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
