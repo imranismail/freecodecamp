9e0358fd10db3d1a4f0f905d1210f79b7a8f8afa
Imran Ismail <imran.codely@gmail.com>
2016-02-21 18:36:44 +0800

    separate logic to class

--- exact_change.js
+++ exact_change.js
@@ -2,54 +2,55 @@
 class Register {
   constructor(cid) {
     this.total = 0;
-    cid.forEach(denom => {
-      this[denom[0]] = denom[1];
-      this.total += denom[1];
+    this.change = 0;
+    this.denoms = [
+      { name: 'ONE HUNDRED', val: 100.00},
+      { name: 'TWENTY', val: 20.00},
+      { name: 'TEN', val: 10.00},
+      { name: 'FIVE', val: 5.00},
+      { name: 'ONE', val: 1.00},
+      { name: 'QUARTER', val: 0.25},
+      { name: 'DIME', val: 0.10},
+      { name: 'NICKEL', val: 0.05},
+      { name: 'PENNY', val: 0.01}
+    ];
+
+    cid.forEach(cash => {
+      this.total += cash[1];
+      this[cash[0]] = cash[1];
     });
   }
 
-  withdraw(denom) {
-    this[denom.name] -= denom.val;
-    this.total       -= denom.val;
-    return denom.val;
+  getChange(amount) {
+    if (this.total === amount) return "Closed";
+    if (this.total < amount) return "Insufficient Funds";
+
+    this.change = amount;
+
+    const changes = this.denoms.reduce((acc, denom) => {
+      var value = 0;
+
+      while (this[denom.name] > 0 && this.change >= denom.val) {
+        this.change      -= denom.val;
+        this[denom.name] -= denom.val;
+        value            += denom.val;
+        this.change       = Math.round(this.change * 100) / 100;
+      }
+
+      if (value > 0) acc.push([denom.name, value]);
+
+      return acc;
+    }, []);
+
+    return changes.length < 1 || this.change > 0 ? "Insufficient Funds" : changes;
   }
 }
 
-const denoms = [
-  { name: 'ONE HUNDRED', val: 100.00},
-  { name: 'TWENTY', val: 20.00},
-  { name: 'TEN', val: 10.00},
-  { name: 'FIVE', val: 5.00},
-  { name: 'ONE', val: 1.00},
-  { name: 'QUARTER', val: 0.25},
-  { name: 'DIME', val: 0.10},
-  { name: 'NICKEL', val: 0.05},
-  { name: 'PENNY', val: 0.01}
-];
-
 function drawer(price, cash, cid) {
-  var change     = cash - price;
   const register = new Register(cid);
+  const change   = register.getChange(cash - price);
 
-  if (register.total === change) return "Closed";
-  if (register.total < change) return "Insufficient Funds";
-
-  const arrOfChange = denoms.reduce((acc, denom) => {
-    var value = 0;
-
-    while (register[denom.name] > 0 && change >= denom.val) {
-      change -= register.withdraw(denom);
-      value  += denom.val;
-    }
-
-    if (value > 0) acc.push([denom.name, value]);
-
-    return acc;
-  }, []);
-
-  if (arrOfChange.length < 1 || change > 0) return "Insufficient Funds";
-
-  return arrOfChange;
+  return change;
 }
 
-drawer(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+drawer(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
