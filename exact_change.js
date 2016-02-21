891e41d3d17c8b7400d48de69fb9e38d31f70f64
Imran Ismail <imran.codely@gmail.com>
2016-02-21 16:44:52 +0800

    fix result

--- exact_change.js
+++ exact_change.js
@@ -1,5 +1,5 @@
 // jshint esversion:6
-class Drawer {
+class Register {
   constructor(cid) {
     this.total = 0;
     cid.forEach(denom => {
@@ -10,34 +10,35 @@ class Drawer {
 
   withdraw(denom) {
     this[denom.name] -= denom.val;
+    this.total       -= denom.val;
     return denom.val;
   }
 }
 
+const denoms = [
+  { name: 'ONE HUNDRED', val: 100.00},
+  { name: 'TWENTY', val: 20.00},
+  { name: 'TEN', val: 10.00},
+  { name: 'FIVE', val: 5.00},
+  { name: 'ONE', val: 1.00},
+  { name: 'QUARTER', val: 0.25},
+  { name: 'DIME', val: 0.10},
+  { name: 'NICKEL', val: 0.05},
+  { name: 'PENNY', val: 0.01}
+];
+
 function drawer(price, cash, cid) {
-  var change   = cash - price;
-  const drawer = new Drawer(cid);
+  var change     = cash - price;
+  const register = new Register(cid);
 
-  if (drawer.total < change) return "Insufficient Funds";
-  if (drawer.total === change) return "Closed";
-
-  const denoms = [
-    { name: 'ONE HUNDRED', val: 100.00},
-    { name: 'TWENTY', val: 20.00},
-    { name: 'TEN', val: 10.00},
-    { name: 'FIVE', val: 5.00},
-    { name: 'ONE', val: 1.00},
-    { name: 'QUARTER', val: 0.25},
-    { name: 'DIME', val: 0.10},
-    { name: 'NICKEL', val: 0.05},
-    { name: 'PENNY', val: 0.01}
-  ];
+  if (register.total === change) return "Closed";
+  if (register.total < change) return "Insufficient Funds";
 
   const arrOfChange = denoms.reduce((acc, denom) => {
     var value = 0;
 
-    while (drawer[denom.name] > 0 && change >= denom.val) {
-      change -= drawer.withdraw(denom).toFixed(2);
+    while (register[denom.name] > 0 && change >= denom.val) {
+      change -= register.withdraw(denom);
       value  += denom.val;
     }
 
@@ -46,11 +47,9 @@ function drawer(price, cash, cid) {
     return acc;
   }, []);
 
-  if (arrOfChange.length < 1 || change > 0) {
-    return "Insufficient Funds";
-  }
+  if (arrOfChange.length < 1 || change > 0) return "Insufficient Funds";
 
   return arrOfChange;
 }
 
-drawer(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+drawer(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
